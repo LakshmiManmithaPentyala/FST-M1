2ccb7bc9e12769b549f19f5d300b82a2f67d984c
LakshmiManmithaPentyala <101718816+LakshmiManmithaPentyala@users.noreply.github.com>
2022-05-01 22:51:37 +0530

    Selenium Activities

---
+++ Selenium/Activities/Hello.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://google.com");
+		driver.close();
+		
+	}
+
+}
